6dfd9080342d51b7a4d6412e929657cc4d70a46c
Majd Sabik <majdsabik.ms@gmail.com>
2020-07-25 19:33:32 +0200

    Implemented autocomplete and delivery map

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import './App.css';
-import { Link } from 'react-router-dom';
 import { googleLogin as Google } from './services/auth';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import MapViewer from './components/MapViewer';
 
 export default class App extends React.Component {
   state = {
     user: this.props.user,
+    message: 'Just a test message',
   };
+
   setUser = user => {
     this.setState({
       user: user,
     });
   };
+
   render = () => {
     return (
       <div className='App'>
-        <Route
-          exact
-          path='/signup'
-          render={props => <Signup setUser={this.setUser} {...props} />}
-        />
-        <Route
-          exact
-          path='/login'
-          render={props => <Login setUser={this.setUser} {...props} />}
-        />
+        <MapViewer />
+        <Route exact path='/signup' render={props => <Signup setUser={this.setUser} {...props} />} />
+        <Route exact path='/login' render={props => <Login setUser={this.setUser} {...props} />} />
       </div>
     );
   };
