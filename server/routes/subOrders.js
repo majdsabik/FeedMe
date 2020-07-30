52553e80fc750f7717bd4d3a9fd9b45fe36eb17d
Ritesh <tauraniritesh@gmail.com>
2020-07-30 11:13:25 +0200

    conflicts resolved

--- server/routes/subOrders.js
+++ server/routes/subOrders.js
@@ -1,25 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const SubOrder = require('../models/SubOrder');
+const SubOrder = require("../models/SubOrder");
 
-router.post('/', (req, res) => {
-  SubOrder.find({ $and: [{ restaurantPrefix: req.body.restaurantPrefix }, { status: { $ne: 'Delivered' } }] })
-    .populate('items')
+router.post("/", (req, res) => {
+  SubOrder.find({
+    $and: [
+      { restaurantPrefix: req.body.restaurantPrefix },
+      { status: { $ne: "Delivered" } },
+    ],
+  })
+    .populate("items")
     .sort({ createdAt: 1 })
-    .then(menu => {
+    .then((menu) => {
       res.status(200).json(menu);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
-router.put('/advance', (req, res) => {
-  SubOrder.findByIdAndUpdate({ _id: req.body.id }, { status: req.body.status }, { upsert: true })
-    .then(menu => {
+router.put("/advance", (req, res) => {
+  SubOrder.findByIdAndUpdate(
+    { _id: req.body.id },
+    { status: req.body.status },
+    { upsert: true }
+  )
+    .then((menu) => {
       res.status(200).json(menu);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
