ec230409dfa398e67920b2d8d90e6430790e068b
Majd Sabik <majdsabik.ms@gmail.com>
2020-07-30 16:14:25 +0200

    Third build

---
+++ client/build/precache-manifest.f5cfaa7753e2dba787f8939ddf596a2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b432b5bf4d7d6245c9449625ee7b841d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d05d59bc0c59bc2a69e3",
+    "url": "/static/css/main.c1f4bc1a.chunk.css"
+  },
+  {
+    "revision": "b57ee913406d15553b69",
+    "url": "/static/js/2.7a4fd734.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/static/js/2.7a4fd734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05d59bc0c59bc2a69e3",
+    "url": "/static/js/main.a8e982bc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "605377a83baf6904979d9cb4479f0c88",
+    "url": "/static/media/Landing.605377a8.png"
+  },
+  {
+    "revision": "4a29cf31dfcc4085440e04516ef7ed93",
+    "url": "/static/media/cloud.4a29cf31.svg"
+  }
+]);
